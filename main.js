0c564266bd7fd2b7d7bf150dffb97615b2164ea9
lislis <mail@lislis.de>
2017-05-23 14:52:18 +0200

    :sparkles:

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png')})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
